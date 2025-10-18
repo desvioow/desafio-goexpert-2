7d53d9d9e48c216b60b99362b5aeb5ca1d46e17d
desvioow <deliberato.lucas@gmail.com>
2025-10-18 10:33:20 -0300

    feat: enhance CLI tool with required flags for functionality
    - Update `rootCmd` with URL, requests, and concurrency flags.
    - Improve `go.mod` and `go.sum` with updated dependencies.

--- cmd/root.go
+++ cmd/root.go
@@ -1,9 +1,7 @@
-/*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -12,16 +10,25 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "desafio-goexpert-2",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "A simple stress test cli tool",
+	Long: `A simple stress test cli tool that can be used to test the performance of a web service and generate a report.
+It achieves this by sending a number of requests to the web service and measuring the time it takes to receive a response.`,
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+
+		url, err := cmd.Flags().GetString("url")
+		requests, err := cmd.Flags().GetInt("requests")
+		concurrency, err := cmd.Flags().GetInt("concurrency")
+
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("Testing URL: %s\n", url)
+		fmt.Printf("Requests: %d\n", requests)
+		fmt.Printf("Concurrency: %d\n", concurrency)
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -42,5 +49,12 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().StringP("url", "u", "", "URL to test")
+	rootCmd.MarkFlagRequired("url")
+
+	rootCmd.Flags().IntP("requests", "r", 0, "Amount of requests to send")
+	rootCmd.MarkFlagRequired("requests")
+
+	rootCmd.Flags().IntP("concurrency", "c", 0, "Concurrency level")
+	rootCmd.MarkFlagRequired("concurrency")
 }
--- main.go
+++ main.go
@@ -1,6 +1,3 @@
-/*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-*/
 package main
 
 import "desafio-goexpert-2/cmd"
